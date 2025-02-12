e495afbb549f6a267bfc945f24745d4f3ceed459
Bibhu Mishra <v-bimishra@outlook.com>
2025-02-12 05:37:37 -0600

    added classification

--- langchain/classification.py
+++ langchain/classification.py
@@ -1,3 +1,14 @@
+############## PROBLEM STATEMENT
+### We have received 1000s of user queries at the customer support center. 
+### Our objective is classify the problem into 3 major categories so that we can address them on priority.
+############## PROBLEM SOLVING APPROACH
+### Step1: take a sample from the queries received ../_data/customer_intent_raw.csv. Ask LLM to find 3 top categories for you. (do multiple run to find the most frequent occuring onces)
+###
+### Step2: take another sample of data, and label them MANUALLY using the 3 categories you found, label other where there is no match.
+###
+### Step3: Use the labelled sample data to determine which prompt technique you should use to categories the query. 
+### Basically, you are going to evaluate prompt techniques.
+
 import json
 import random
 import session_info
@@ -12,33 +23,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from utility.llm import LLM
 
-user_message_template = """```{product_description}```"""
+from langchain_core.output_parsers import StrOutputParser
+parser = StrOutputParser()
+llm = LLM.get_client()
+chain = llm | parser
 
-few_shot_system_message = """
-    Classify the following product desciption presented in the input into one of the following categories.
-    Categories - ['Hair Care', 'Skin Care']
+class Classification:
+
+    @staticmethod
+    def find_top_5_category():
+        dataset = pd.read_csv("../_data/customer_intent_raw.csv")
+
+        raw_intent = dataset['description'].str.cat(sep=' || ')
+
+        prompt = f"""Extract the list of problems faced by the user from the following customer queries, separated by " || ". A problem category should be a two word label. List out 10 unique problems
+        Then, identify the top 5 most frequent problem encountered by the user.
+
+        Customer queries:
+        {raw_intent}"""
+
+        messages = [{'role':'user','content':prompt}]
+
+        response = chain.invoke(messages,
+                    config={"temperature": 0, "seed": 49})
+
+        print(response)
+
+user_message_template = """```{description}```"""
+
+few_shot_system_message = """Classify the following product desciption presented in the input into one of the following categories.
+    Categories - ['change order', 'track order', 'payment issue']
     Product description will be delimited by triple backticks in the input.
-    Answer only 'Hair Care' or 'Skin Care'. Do not explain your answer.
+    Answer only 'change order', or 'track order', or 'payment issue'. Nothing Else. Do not explain your answer.
 """
-zero_shot_system_message = """
-    Classify the following product desciption presented in the input into one of the following categories.
-    Categories - ['Hair Care', 'Skin Care']
+zero_shot_system_message = """Classify the following product desciption presented in the input into one of the following categories.
+    Categories - ['change order', 'track order', 'payment issue', 'others']
     Product description will be delimited by triple backticks in the input.
-    Answer only 'Hair Care' or 'Skin Care'. Nothing Else. Do not explain your answer.
-"""
-
-cot_system_message = """
-    Given the following product description, follow these steps to determine the appropriate product label category:
-
-    1. Read the product description carefully, looking for key words and phrases that indicate the product's purpose and usage.
-
-    2. Consider if the description mentions any particular keywords relating to a category.
-
-    3. If the description contains keywords related to multiple categories, determine which category is most strongly emphasized or which usage is primary.
-
-    4. If the description does not contain any clear keywords related to the given categories, consider the overall context and purpose of the product to make an educated guess about the most appropriate category.
-
-    5. Output the determined category label ( 'Hair Care', or 'Skin Care') and nothing else. Do not explain your output.
+    Answer only 'change order',
+    or 'track order', or 'payment issue',or 'others'. Nothing Else. Do not explain your answer.
 """
 
 class PropmtEvaluator:
@@ -79,24 +101,25 @@ class PropmtEvaluator:
 
         Args:
             prompt (List): list of messages in the Open AI prompt format
-            gold_test_data (str): JSON string with list of gold examples
-            user_message_template (str): string with a placeholder for product description
+            gold_examples (str): JSON string with list of gold examples
+            user_message_template (str): string with a placeholder for description.
             samples_to_output (int): number of sample predictions and ground truths to print
 
         Output:
-            accuracy (float): Accuracy computed by comparing model predictions
+            accuracy (float): accuracy score computed by comparing model predictions
                                     with ground truth
         """
-
-        count =0
+        count = 0
         model_predictions, ground_truths = [], []
 
+        # Iterating through all the gold examples and constructing the messages dictionary using the text from example
+
         for example in json.loads(gold_test_data):
-            gold_input = example['Product Description']
+            gold_input = example['description']
             user_input = [
                 {
                     'role':'user',
-                    'content': user_message_template.format(product_description=gold_input)
+                    'content': user_message_template.format(description=gold_input)
                 }
             ]
 
@@ -105,35 +128,56 @@ class PropmtEvaluator:
                 #     model=deployment_name,
                 #     messages=prompt+user_input,
                 #     temperature=0, # <- Note the low temperature
-                #     max_tokens=4 # <- Note how we restrict the output to not more than 4 tokens
+                #     max_tokens=4 # <- Note how we restrict the output to not more than 2 tokens
                 # )
 
                 # prediction = response.choices[0].message.content
-
                 prediction=PropmtEvaluator.get_chain().invoke(prompt+user_input,
                     config={"temperature": 0.5, "max_tokens": 4})
-
-                # print(prediction) #uncomment to see LLM response or to debug
-                model_predictions.append(prediction.strip().lower()) # <- removes extraneous white space and lowercases output
-                ground_truths.append(example['Category'].strip().lower())
-
-                if count < samples_to_output:
+                while count < samples_to_output:
                     count += 1
-                print("Product Description: \n", example['Product Description'],"\n")
-                print("Original label: \n", example['Category'],"\n")
+                print("Original label: \n", example['task'],"\n")
                 print("Predicted label: \n", prediction)
-                print("====================================================")
+
+
+                model_predictions.append(prediction.strip().lower()) # <- removes extraneous white space and lowercases output
+                ground_truths.append(example['task'].strip().lower())
 
             except Exception as e:
                 print(e)
                 continue
 
-            accuracy = accuracy_score(ground_truths, model_predictions)
+
+        # Find the accuracy of each category.
+
+        df = pd.DataFrame({
+        'Predictions': model_predictions,
+        'Ground Truth': ground_truths
+        })
+        labels = df['Ground Truth'].unique()
+
+        # Create a new DataFrame for the accuracies
+        accuracy_df = pd.DataFrame(columns=labels)
+
+        for label in labels:
+            # Filter rows where Ground Truth is the current label
+            subset = df[df['Ground Truth'] == label]
+
+            # Calculate accuracy for the current label
+            accuracy = accuracy_score(subset['Ground Truth'], subset['Predictions'])
+
+            # Add accuracy to the DataFrame
+            accuracy_df.loc[0, label] = accuracy
+
+        print("\n\n", accuracy_df)
+        print("====================================================")
+
+        accuracy = accuracy_score(ground_truths, model_predictions)
 
         return accuracy
 
     @staticmethod
-    def __create_examples(dataset, n=4):
+    def __create_examples(examples_df, n=4):
 
         """
         Return a JSON list of randomized examples of size 2n with two classes.
@@ -150,16 +194,18 @@ class PropmtEvaluator:
             randomized_examples (JSON): A JSON with examples in random order
         """
 
-        hc_reviews = (examples_df.Category == 'Hair Care')
-        sc_reviews = (examples_df.Category == 'Skin Care')
+        task1 = (examples_df.task == 'change order')
+        task2 = (examples_df.task == 'track order')
+        task3 = (examples_df.task == 'payment issue')
+        task4 = (examples_df.task == 'others')
 
-        cols_to_select = ["Product Description","Category"]
-        hc_examples = examples_df.loc[hc_reviews, cols_to_select].sample(n)
-        sc_examples = examples_df.loc[sc_reviews, cols_to_select].sample(n)
-
-        examples = pd.concat([hc_examples,sc_examples])
+        t1_examples = examples_df.loc[task1, :].sample(n)
+        t2_examples = examples_df.loc[task2, :].sample(n)
+        t3_examples = examples_df.loc[task3, :].sample(n)
+        t4_examples = examples_df.loc[task4, :].sample(n)
+        examples = pd.concat([t1_examples,t2_examples,t3_examples,t4_examples])
         # sampling without replacement is equivalent to random shuffling
-        randomized_examples = examples.sample(2*n, replace=False)
+        randomized_examples = examples.sample(4*n, replace=False)
 
         return randomized_examples.to_json(orient='records')
 
@@ -183,20 +229,18 @@ class PropmtEvaluator:
         few_shot_prompt = [{'role':'system', 'content': system_message}]
 
         for example in json.loads(examples):
-            example_description = example['Product Description']
-            example_category = example['Category']
 
             few_shot_prompt.append(
                 {
                     'role': 'user',
                     'content': user_message_template.format(
-                        product_description=example_description
+                        description=example['description']
                     )
                 }
             )
 
             few_shot_prompt.append(
-                {'role': 'assistant', 'content': f"{example_category}"}
+                {'role': 'assistant', 'content': f"{example['task']}"}
             )
 
         return few_shot_prompt
@@ -264,32 +308,5 @@ class PropmtEvaluator:
         print(f"Standard deviation in few-shot-prompt performance {few_shot_std}.")
         print(f"Standard deviation in cot-few-shot-prompt performance {cot_few_shot_std}.")
 
-
 if __name__ == "__main__":
-
-    ## Prepare data for example and testing
-    data = pd.read_csv("../_data/auto-labelling.csv")
-    examples_df, gold_test_df = train_test_split(
-        data, #<- the full dataset
-        test_size=0.8, #<- 80% random sample selected for gold examples
-        random_state=42, #<- ensures that the splits are the same for every session
-        stratify=data['Category'] #<- ensures equal distribution of labels
-    )
-
-    gold_test_data = (
-        gold_test_df.to_json(orient='records')
-    )
-
-    # token_size, accuracy = PropmtEvaluator.evaluate_zero_shot_promt(gold_test_data)
-    # print(f"zero-shot-promt token size is {token_size} and accuracy is {accuracy}")
-
-
-    # token_size, accuracy = PropmtEvaluator.evaluate_few_shot_promt(examples_df,gold_test_data)
-    # print(f"few-shot-promt token size is {token_size} and accuracy is {accuracy}")
-
-
-    # token_size, accuracy = PropmtEvaluator.evaluate_cot_promt(examples_df,gold_test_data)
-    # print(f"cot-promt token size is {token_size} and accuracy is {accuracy}")
-
-    PropmtEvaluator.compare_few_shot_vs_cot_promt(examples_df,gold_test_data)
-
+    Classification.find_top_5_category()
